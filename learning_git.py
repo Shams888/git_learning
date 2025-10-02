ce1d7f039ef25dc0557df9a5e9539de3d18777b8
Shams <shams@cbnu.ac.kr>
2025-10-02 12:43:19 +0900

    feat : add some variable sum and difference

--- learning_git.py
+++ learning_git.py
@@ -1,3 +1,7 @@
 print("Learning Git")
 print("This is a simple script to demonstrate Git usage.")
 print("You can modify this script and track changes using Git.")
+a=5
+b=10 
+print("Sum:", a + b)
+print("Difference:", b - a)
